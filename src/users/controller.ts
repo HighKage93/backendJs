450842c0856aeaf3780eae9a318b7858218e879b
Mayur <mayur@BLR-PFH-LAP1050.local>
2022-12-03 17:14:47 +0530

    .

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -1,10 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 
 @Controller('user')
 export class UserController {
   @Get()
   findAll(): string {
+    return 'This action returns all cats';
+  }
+
+  @Post()
+  saveUser(@Req() req: any): string {
+    console.log(req)
     return 'This action returns all cats';
   }
 }
