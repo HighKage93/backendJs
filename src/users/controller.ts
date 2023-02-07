51a0e39efd9e1d42e107a8ff1ebf9e7e6825a709
mayur-patankar <patankarm06@gmail.com>
2023-02-08 02:19:36 +0530

    .

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { User } from './entity';
 import { UserI } from './interfaces/userinterface';
 import { UsersService } from './services';
 
@@ -9,7 +10,22 @@ export class UserController {
   constructor(private userService: UsersService) {}
 
   @Get()
-  async findAll(): Promise<UserI[]> {
+  async findAll(): Promise<User[]> {
     return this.userService.findAll();
+    // return 'Some dumb dumbness'
+  }
+
+  @Post()
+  async create(@Body() createUser: UserI): Promise<UserI> {
+    console.log(createUser, ' create user ')
+    // return this.userService.findAll();
+    return createUser;
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string): Promise<string> {
+    console.log(id, ' create user ')
+    // return this.userService.findAll();
+    return id;
   }
 }
